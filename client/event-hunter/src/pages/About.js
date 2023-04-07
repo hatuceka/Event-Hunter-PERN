28f3f5e06d9f4e3d242f7879edceb8940c36ec39
Hatice Yorukoglu <yorukogluhatice@gmail.com>
2023-04-07 11:50:54 -0400

    About page css is set

--- client/event-hunter/src/pages/About.js
+++ client/event-hunter/src/pages/About.js
@@ -1,5 +1,50 @@
+import '../style/about.css'
+
 const About = () => {
-  return <div></div>
+  return (
+    <div className="about">
+      <p>
+        Welcome to Event Hunter, your one-stop solution for tracking and
+        discovering exciting events in your area. Whether you're looking for
+        concerts, sports games, conferences, festivals, or any other type of
+        event, Event Hunter has got you covered.
+      </p>
+      <p>
+        Our app is designed to make it easy for you to stay up-to-date with the
+        latest happenings in your city or region. You can search for events
+        based on your interests, location, or date, and filter the results by
+        category, price, and other parameters. You can also save events to your
+        calendar, get reminders, and share them with your friends.
+      </p>
+
+      <p>
+        One of the unique features of Event Hunter is its personalized
+        recommendations engine. Based on your search history and preferences,
+        the app suggests events that are likely to be of interest to you. This
+        helps you discover new events that you might not have found otherwise,
+        and ensures that you never miss out on something that you would have
+        loved to attend.
+      </p>
+      <p>
+        Another key aspect of Event Hunter is its community of users. You can
+        join groups based on your interests, connect with like-minded people,
+        and get access to exclusive events and discounts. You can also follow
+        your favorite venues, artists, and organizers, and get updates on their
+        upcoming events.
+      </p>
+      <p>
+        At Event Hunter, we are committed to making the event discovery and
+        tracking process as seamless and enjoyable as possible. Our app is
+        intuitive, easy to use, and constantly updated with new features and
+        improvements. We value your feedback and suggestions, and strive to
+        provide you with the best possible experience.
+      </p>
+      <p>
+        Thank you for choosing Event Hunter, and we look forward to helping you
+        find your next unforgettable event.
+      </p>
+    </div>
+  )
 }
 
 export default About
