9e2da4b54e59bb5d230727b3f94a0b5427f7f4f2
Hatice Yorukoglu <yorukogluhatice@gmail.com>
2023-04-08 00:26:10 -0400

    OrderPlaced css is done

--- client/event-hunter/src/pages/OrderPlaced.js
+++ client/event-hunter/src/pages/OrderPlaced.js
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom'
+
 const OrderPlaced = () => {
   return (
-    <div>
-      <h1>Your tickets is ready</h1>
+    <div className="cartBody">
+      <h1 className="placed">
+        Your ticket is ready!
+        <br></br>
+        Enjoy the event!
+      </h1>
+
+      <Link className="goBackPlaced" to="/">
+        Go Back to Hunt Events
+      </Link>
     </div>
   )
 }
