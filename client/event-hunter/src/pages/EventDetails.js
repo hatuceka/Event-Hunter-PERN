7ebc640f0c4dc7c1bd6faded32950fc36b335fa1
Hatice Yorukoglu <yorukogluhatice@gmail.com>
2023-04-08 00:16:21 -0400

    Event detail page css is fixed.

--- client/event-hunter/src/pages/EventDetails.js
+++ client/event-hunter/src/pages/EventDetails.js
@@ -1,3 +1,4 @@
+import '../style/detail.css'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
@@ -44,22 +45,37 @@ const EventDetails = ({ user, props, addToCart }) => {
 
   if (eventDetails.title)
     return (
-      <div>
-        <h1>{eventDetails.title}</h1>
-        <img src={eventDetails.performers[0].image} />
-        <h2>{eventDetails.type}</h2>
-        <h2>{eventDetails.datetime_local}</h2>
-        <h2>
-          {eventDetails.venue.city} {eventDetails.venue.state}{' '}
-          {eventDetails.venue.name}
-        </h2>
-        <Link to="/">Go back to hunt events</Link>
-
-        {user ? (
-          <button onClick={() => addToCart(eventDetails)}>Add to Cart</button>
-        ) : (
-          <h2>Go to LOGIN to buy tickets!</h2>
-        )}
+      <div className="detailDiv">
+        <img className="eventImage" src={eventDetails.performers[0].image} />
+        <div className="detailInfo">
+          <h1 className="detailTitle">
+            <span className="span">Name:</span> {eventDetails.title}
+          </h1>
+          <h2 className="detailTitle">
+            <span className="span">Type:</span> {eventDetails.type}
+          </h2>
+          <h2 className="detailTitle">
+            <span className="span"> Date and Time:</span>{' '}
+            {eventDetails.datetime_local}
+          </h2>
+          <h2 className="detailTitle">
+            <span className="span">Place:</span>
+            {eventDetails.venue.city} {eventDetails.venue.state}{' '}
+            {eventDetails.venue.name}
+          </h2>
+        </div>
+        <div className="detailBtns">
+          {user ? (
+            <button className="add" onClick={() => addToCart(eventDetails)}>
+              Add to Cart
+            </button>
+          ) : (
+            <h2 className="detailTitle">Go to LOGIN to buy tickets!</h2>
+          )}
+          <Link className="goBack" to="/">
+            Go Back to Hunt Events
+          </Link>
+        </div>
       </div>
     )
 }
